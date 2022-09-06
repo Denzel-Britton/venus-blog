cea3170a3303d66baca919840db1c01cd712721d
Denzel-Britton <denzel.britton573@gmail.com>
2022-09-06 15:03:57 -0500

    first commit

---
+++ src/main/java/com/example/venusblog/VenusBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.venusblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenusBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VenusBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/venusblog/VenusBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.venusblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenusBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
