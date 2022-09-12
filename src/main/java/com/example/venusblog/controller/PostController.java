5c0a838159cc7f323173fffa5c3df99c0dbfeff4
Denzel-Britton <denzel.britton573@gmail.com>
2022-09-12 14:37:20 -0500

    working on usersname function

--- src/main/java/com/example/venusblog/controller/PostController.java
+++ src/main/java/com/example/venusblog/controller/PostController.java
@@ -15,77 +15,78 @@ import java.util.List;
 
 public class PostController {
     private List<Post> posts = new ArrayList<>();
+    private long nextId = 1;
 
-
-        @GetMapping("")
+    @GetMapping("")
 //    @RequestMapping(value = "/", method = RequestMethod.GET)
     public List<Post> fetchPosts() {
-
-
+        System.out.println("Hey I made a change!");
         return posts;
     }
 
+    @GetMapping("/{id}")
+    public Post fetchPostById(@PathVariable long id) {
+        // search through the list of posts
+        // and return the post that matches the given id
+        Post post = findPostById(id);
+        if(post == null) {
+            // what to do if we don't find it
+            throw new RuntimeException("I don't know what I am doing");
+        }
 
+        // we found the post so just return it
+        return post;
+    }
 
-        @GetMapping("/{id}")
-        public Post fetchPostsById ( @PathVariable long id){
-            //search through the list of post and return the post that matches the given id
-            for (Post post:posts) {
-               if (post.getId()== id) {
-                   return post;
-               }
+    private Post findPostById(long id) {
+        for (Post post: posts) {
+            if(post.getId() == id) {
+                return post;
             }
-            throw new  RuntimeException("try again");
         }
+        // didn't find it so do something
+        return null;
+    }
 
-        private Post findPostById(long id) {
-            for (Post post : posts) {
-                if (post.getId() == id) {
-                    return post;
-                }
+    @PostMapping("")
+    public void createPost(@RequestBody Post newPost) {
+//        System.out.println(newPost);
+        // assign  nextId to the new post
+        newPost.setId(nextId);
+        nextId++;
 
+        posts.add(newPost);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePostById(@PathVariable long id) {
+        // search through the list of posts
+        // and delete the post that matches the given id
+        Post post = findPostById(id);
+        if(post != null) {
+            posts.remove(post);
+            return;
+        }
+        // what to do if we don't find it
+        throw new RuntimeException("Post not found");
+    }
+
+    @PutMapping("/{id}")
+    public void updatePost(@RequestBody Post updatedPost, @PathVariable long id) {
+        // find the post to update in the posts list
+
+        Post post = findPostById(id);
+        if(post == null) {
+            System.out.println("Post not found");
+        } else {
+            if(updatedPost.getTitle() != null) {
+                post.setTitle(updatedPost.getTitle());
             }
-            return null;
-        }
-
-        @PostMapping("")
-        private void createPost (@RequestBody Post newPost ){
-            posts.add(newPost);
-
-        }
-        @PutMapping("/{id}")
-        public void updatePost(@RequestBody Post updatedPost, @PathVariable long id) {
-            // find the post to update in the posts list
-
-            Post post = findPostById(id);
-            if(post == null) {
-                System.out.println("Post not found");
-            } else {
-                if(updatedPost.getTitle() != null) {
-                    post.setTitle(updatedPost.getTitle());
-                }
-                if(updatedPost.getContent() != null) {
-                    post.setContent(updatedPost.getContent());
-                }
-                return;
+            if(updatedPost.getContent() != null) {
+                post.setContent(updatedPost.getContent());
             }
-            throw new RuntimeException("Post not found");
+            return;
         }
-
-
-        @DeleteMapping("/{id}")
-        public void deletePostById (@PathVariable Long id){
-//            System.out.println("delete function");
-//            search through the list of posts and delete
-            for (Post post:posts) {
-                if (post.getId()== id) {
-                    //if we fin the post then delete it
-                    posts.remove(post);
-                    return; // so you don't run into the throw new exceptions
-                }
-            }
-            throw new  RuntimeException("try again");
-        }
-
-        }
-
+        throw new RuntimeException("Post not found");
+    }
+}
---
+++ src/main/java/com/example/venusblog/data/UserRole.java
@@ -0,0 +1,5 @@
+package com.example.venusblog.data;
+
+public enum UserRole {
+    USER, ADMIN
+}
