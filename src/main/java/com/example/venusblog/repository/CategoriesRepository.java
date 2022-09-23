730fbd3f50104442eae5554639752044cf1de8d2
Denzel-Britton <denzel.britton573@gmail.com>
2022-09-23 14:36:21 -0500

    update controllers and data tables

--- src/main/java/com/example/venusblog/repository/CategoriesRepository.java
+++ src/main/java/com/example/venusblog/repository/CategoriesRepository.java
@@ -3,6 +3,6 @@ package com.example.venusblog.repository;
 import com.example.venusblog.data.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoriesRepository extends JpaRepository<Category, Long> {
+public interface CategoriesRepository extends JpaRepository<Category ,Long> {
 
 }
